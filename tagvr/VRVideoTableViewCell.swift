f21ec017f3bba3e42a0e8279c1bba0a9ec19e012
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-10-03 15:40:22 +1300

    add table view controller

---
+++ tagvr/VRVideoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VRVideoTableViewCell.swift
+//  tagvr
+//
+//  Created by Ira Ritchie Meek on 3/10/16.
+//  Copyright © 2016 Tag the agency. All rights reserved.
+//
+
+import UIKit
+
+class VRVideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

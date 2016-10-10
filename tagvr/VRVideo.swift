7b109f79836a3fac9dbf12878e78e515f5ce70da
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-10-11 11:40:53 +1300

    add video link to vrVideo object

--- tagvr/VRVideo.swift
+++ tagvr/VRVideo.swift
@@ -13,13 +13,14 @@ class VRVideo {
     var photo: UIImage
     var title: String
     var duration: String
+    var video: String
     
-    
-    init?(photo: UIImage, title: String, duration: String) {
+    init?(photo: UIImage, title: String, duration: String, video: String) {
         
         self.photo = photo
         self.title = title
         self.duration = duration
+        self.video = video
 
     }
     

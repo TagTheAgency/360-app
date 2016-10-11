0e67e6fa858906dec5db61d41f9bf523a775f77e
Jared Warren <jared.warren@live.com>
2016-10-12 10:08:04 +1300

    add label for message status

--- tagvr/ContactPageController.swift
+++ tagvr/ContactPageController.swift
@@ -14,12 +14,15 @@ class ContactPageController: UIViewController, MFMailComposeViewControllerDelega
     
     @IBOutlet weak var contactName: UITextField!
     @IBOutlet weak var contactEmail: UITextField!
-    @IBOutlet weak var contactMessage: UITextView!
+    @IBOutlet weak var contactMessage: UITextField!
+    @IBOutlet weak var messageStatus: UILabel!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,37 +31,44 @@ class ContactPageController: UIViewController, MFMailComposeViewControllerDelega
     }
     
     @IBAction func sendMessage(_ sender: AnyObject) {
-        
-        let toRecipients = ["abc@tagtheangecy.com"]
+        let toRecipients = ["jared@tagtheagency.com"]
         let mc: MFMailComposeViewController = MFMailComposeViewController()
-        mc.mailComposeDelegate = self
-        mc.setToRecipients(toRecipients)
-        mc.setSubject(contactName.text!)
-        mc.setMessageBody("Name: \(contactName.text!) \n\nEmail: \(contactEmail) \n\nMessage: \(contactMessage)", isHTML: false)
-        self.present(mc, animated: true, completion: nil)
+        if (MFMailComposeViewController.canSendMail()) {
+            mc.mailComposeDelegate = self
+            mc.setToRecipients(toRecipients)
+            mc.setSubject(contactName.text!)
+            mc.setMessageBody("Name: \(contactName.text!) \n\nEmail: \(contactEmail.text!) \n\nMessage: \(contactMessage.text!)", isHTML: false)
+            self.present(mc, animated: true, completion: nil)
+        }
     }
     
-//    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
-//        
-//        switch result.rawValue {
-//        case MFMailComposeResultCancelled.rawValue:
-//            print()
-//        case MFMailComposeResultFailed.rawValue:
-//            print()
-//        case MFMailComposeResultSaved.rawValue:
-//            print()
-//        case MFMailComposeResultSent.rawValue:
-//            print()
-//        default:
-//            break
-//        }
-//        
-//        self.dismissViewControllerAnimated(true, completion: nil)
-//    }
-    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
+        switch result.rawValue {
+        case MFMailComposeResult.cancelled.rawValue:
+            print("Cancelled")
+        case MFMailComposeResult.failed.rawValue:
+            print("Failed")
+            messageStatus.text = "Message failed to send."
+            messageStatus.layer.borderWidth = 0.5
+            messageStatus.layer.borderColor = UIColor.red.cgColor
+        case MFMailComposeResult.saved.rawValue:
+            print("Saved")
+        case MFMailComposeResult.sent.rawValue:
+            print("Sent")
+            messageStatus.text = "Message sent, thanks!"
+            messageStatus.layer.borderWidth = 1
+            messageStatus.layer.borderColor = UIColor.green.cgColor
+        default:
+            break
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
     
     
     @IBAction func dismissKeyboard(_ sender: AnyObject) {
         self.resignFirstResponder()
     }
+    
 }

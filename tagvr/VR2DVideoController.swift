86ddebc119d5ba674e099000dfba03a5bc12febc
Jared Warren <jared.warren@live.com>
2016-10-12 15:35:53 +1300

    add controllers for 2D video

---
+++ tagvr/VR2DVideoController.swift
@@ -0,0 +1,46 @@
+//
+//  VR2DVideoController.swift
+//  tagvr
+//
+//  Created by Tag User #1 on 12/10/16.
+//  Copyright © 2016 Tag the agency. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class VR2DVideoController: UIViewController {
+    
+    
+    @IBOutlet weak var videoView: UIView!
+    
+    @IBOutlet weak var detailImageView: UIImageView!
+    @IBOutlet weak var detailDuration: UILabel!
+    @IBOutlet weak var detailTitle: UILabel!
+    
+    var vrVideo: VRVideo?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        if let vrVideo = vrVideo {
+            let fileURL: NSURL? = NSURL(string: vrVideo.video)
+            let player = AVPlayer(url: fileURL! as URL)
+            let playerLayer = AVPlayerLayer(player: player)
+            playerLayer.frame = self.view.bounds
+            self.videoView.layer.addSublayer(playerLayer)
+            player.play()
+
+            detailImageView.image = vrVideo.photo
+            detailTitle.text = vrVideo.title
+            detailDuration.text = vrVideo.duration
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}

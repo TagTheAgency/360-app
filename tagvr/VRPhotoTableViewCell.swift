c507b1adf97c6a712517f408a2e0d0f7629477ab
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-10-12 16:16:38 +1300

    remove unused images, add 360 photos and add VRPhotoTableViewController

---
+++ tagvr/VRPhotoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VRPhotoTableViewCell.swift
+//  tagvr
+//
+//  Created by Ira Ritchie Meek on 12/10/16.
+//  Copyright © 2016 Tag the agency. All rights reserved.
+//
+
+import UIKit
+
+class VRPhotoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

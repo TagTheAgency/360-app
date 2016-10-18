18630cb2843dce9c2ddefe2e1070e88c5dc0246d
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-10-18 13:50:01 +1300

    update views

--- tagvr/ViewController.swift
+++ tagvr/ViewController.swift
@@ -10,12 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    @IBOutlet weak var detailImageView: UIImageView!
-//    @IBOutlet weak var detailTitle: UILabel!
-//    @IBOutlet weak var detailDuration: UILabel!
-//    @IBOutlet weak var videoVRView: GVRVideoView!
     @IBOutlet weak var menuPanoView: GVRPanoramaView!
-    
     
     var vrVideo: VRVideo?
     
@@ -23,18 +18,7 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         
-//        self.menuPanoView.load(UIImage(named: "tokyo360")!, of: GVRPanoramaImageType.mono)
-        
-        
-//        if let vrVideo = vrVideo {
-//            videoVRView.load(from: URL(string: vrVideo.video))
-//            videoVRView.enableCardboardButton = true
-//            videoVRView.enableFullscreenButton = true
-//            detailImageView.image = vrVideo.photo
-//            detailTitle.text = vrVideo.title
-//            detailDuration.text = vrVideo.duration
-//        }
-        
+        self.menuPanoView.load(UIImage(named: "tokyo360")!, of: GVRPanoramaImageType.mono)
 
     }
 
